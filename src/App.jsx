bd2ac544b9c4271eadf60cc13969c702506c3326
Juan Malbran <juan@Juans-Air.fibertel.com.ar>
2023-08-01 16:30:02 -0300

    versionBase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const nombre = 'My Tinerary'
+  return (
+    <div className="w-full min-h-screen bg-red-500">
+      <header>esta es la barra de navegacion</header>
+      <main>{'nombre'}</main>
+      <footer>va el footer</footer>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const mode = 'jit';
+export const purge = [
+  './src/**/*.{js,jsx,ts,tsx}'
+];
+export const theme = {
+  extend: {}
+};
+export const variants = {};
+export const plugins = [];
