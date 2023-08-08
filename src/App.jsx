345dce059b31c34495421795ee18b162c5c4f1fe
Juan Jacinto Malbran <93431595+Cloncajurda@users.noreply.github.com>
2023-08-08 10:36:57 -0300

    effects (#2)
    Co-authored-by: Juan Malbran <juan@Juans-Air.fibertel.com.ar>

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,22 @@ import MainLayout from "./layouts/MainLayout"
 import NavLayout from "./layouts/NavLayout"
 import CardPolaroid from "./components/CardPolaroid"
 import Carousel from "./components/Carousel"
-import { useState } from "react"
+import { useState,useEffect } from "react"
+import axios from "axios"
 
 function App() {
   const [show,setShow] = useState(true)
+  const [data,setData] = useState([])
   const description = `MyTinerary`
 
-  let data = [
-    {id: 'america1', city: "Cancun", photo: "/img/america/cancun.jpg"},
-    {id: 'america2', city: "New York", photo: "/img/america/newyork.jpg"},
-    {id: 'america3', city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
-    {id: 'america4', city: "Ushuaia", photo: "/img/america/ushuaia.jpg"},
-    {id: 'asia1' , city: "Bangkok", photo: "/img/asia/bangkok.jpg"},
-    {id: 'asia2' , city: "Pekin", photo: "/img/asia/pekin.jpg"},
-    {id: 'asia3' , city: "Singapur", photo: "/img/asia/singapur.jpg"},
-    {id: 'asia4' , city: "Tokyo", photo: "/img/asia/tokio.jpg"},
-    {id: 'europe1' , city: "Ibiza", photo: "/img/europe/ibiza.jpg"},
-    {id: 'europe2' , city: "London", photo: "/img/europe/london.jpg"},
-    {id: 'europe3' , city: "Paris", photo: "/img/europe/paris.jpg"},
-    {id: 'europe4' , city: "Roma", photo: "/img/europe/roma.jpg"},
-    {id: 'oceania1' , city: "Majuro", photo: "/img/oceania/majuro.jpg"},
-    {id: 'oceania2' , city: "Sidney", photo: "/img/oceania/sidney.jpg"},
-    {id: 'oceania3' , city: "Suva", photo: "/img/oceania/suva.jpg"},
-    {id: 'oceania4' , city: "Wellington", photo: "/img/oceania/wellington.jpg"}
-  ]
+  useEffect(
+    ()=>{
+      axios('/data.json')
+      .then(res=>setData(res.data))
+      .catch(err=>console.log(err))
+    },
+    []
+  )
   return (
     <NavLayout>
       <div className="bg-blue-200 p-2 text-end">
